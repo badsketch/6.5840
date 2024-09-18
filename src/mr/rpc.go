006a543c30a051f9d68f7280b21be38d665b077f
Daniel Wang <danielwang405@gmail.com>
2024-09-18 00:12:23 -0400

    Autoformatting

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -20,10 +22,10 @@ type ExampleArgs struct {
 
 type ExampleReply struct {
 	Y int
+	Z int
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
